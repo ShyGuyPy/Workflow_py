2f09608dbdab4553410abb618a7108d886bb20d7
ShyGuyPy <luke.vawter1@gmail.com>
2019-06-13 17:37:17 -0400

    change to csv to account for sqlite not having bool type

--- Aqueduct_test.py
+++ Aqueduct_test.py
@@ -58,53 +58,53 @@ if __name__ == "__main__":
     s = session()
 
 
-#try:
+try:
 
     file_name = "data/Reservoir_intake_retweaked.csv"
     data = Load_Data(file_name)
 
     print(data)
 
-#
-#     for i in data:
-#         record = Data(**{
-#             'year' : i[1],
-#             'month' : i[2],
-#             'res' : i[3],
-#             'solids' : i[4],
-#             'NTU' : i[5],
-#             'org_ml' : i[6],
-#             'MPN_100ml' : i[7],
-#             'MPN_100ml_1' : i[8],
-#             'LosR' : i[9],
-#             'Silica' : i[10],
-#             'Ca' : i[11],
-#             'Mg' : i[12],
-#             'Ca_Mg' : i[13],
-#             'NO3' : i[14],
-#             'NO3_USGS' : i[15],
-#             'Cl2' : i[16],
-#             'Na' : i[17],
-#             'SO4' : i[18],
-#             'K' : i[19],
-#             'pH' : i[20],
-#             'Alk' : i[21],
-#             'Hard' : i[22],
-#             'Nhard' : i[23],
-#             'C_USGSTemp' : i[24],
-#             'F_USGS' : i[25],
-#             'Temp' : i[26],
-#             'MD_Precip_inch_mon' : i[27],
-#             'MD_Temp_F' : i[28]
-#         })
-#         s.add(record)
-#
-#     s.commit()
-#     print("success")
-# except:
-#     s.rollback()
-#     print("no good")
-# finally:
-#     s.close()
-#     print("connection closed")
-# print("Time elapsed: " + str(time() - t) + " s.")
+
+    for i in data:
+        record = Data(**{
+            'year' : i[1],
+            'month' : i[2],
+            'res' : i[3],
+            'solids' : i[4],
+            'NTU' : i[5],
+            'org_ml' : i[6],
+            'MPN_100ml' : i[7],
+            'MPN_100ml_1' : i[8],
+            'LosR' : i[9],
+            'Silica' : i[10],
+            'Ca' : i[11],
+            'Mg' : i[12],
+            'Ca_Mg' : i[13],
+            'NO3' : i[14],
+            'NO3_USGS' : i[15],
+            'Cl2' : i[16],
+            'Na' : i[17],
+            'SO4' : i[18],
+            'K' : i[19],
+            'pH' : i[20],
+            'Alk' : i[21],
+            'Hard' : i[22],
+            'Nhard' : i[23],
+            'C_USGSTemp' : i[24],
+            'F_USGS' : i[25],
+            'Temp' : i[26],
+            'MD_Precip_inch_mon' : i[27],
+            'MD_Temp_F' : i[28]
+        })
+        s.add(record)
+
+    s.commit()
+    print("success")
+except:
+    s.rollback()
+    print("no good")
+finally:
+    s.close()
+    print("connection closed")
+print("Time elapsed: " + str(time() - t) + " s.")

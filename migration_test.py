7882be01db079ce94ef2104da1fceb57936d9c49
ShyGuyPy <luke.vawter1@gmail.com>
2019-06-19 15:57:57 -0400

    working on importing csv to flask db model to migration_test.py

--- migration_test.py
+++ migration_test.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
+from numpy import genfromtxt
+
 import os
 
 #basedir = os.path.abspath(os.path.dirname(__file__))
@@ -51,3 +53,46 @@ class Data(db.Model):
     Temp = db.Column(db.Float)
     MD_Precip_inch_mon = db.Column(db.Float)
     MD_Temp_F = db.Column(db.Float)
+
+
+def Load_Data(file_name):
+    data = genfromtxt(file_name, delimiter=',', skip_header=1, converters={0: lambda s: str(s)})
+    return data.tolist()
+
+file_name = "data/Reservoir_intake_retweaked.csv"
+data = Load_Data(file_name)
+
+#this needs to be adapted to flask-migrate structure
+for i in data:
+    record = Data(**{
+        'year': i[1],
+        'month': i[2],
+        'res': i[3],
+        'solids': i[4],
+        'NTU': i[5],
+        'org_ml': i[6],
+        'MPN_100ml': i[7],
+        'MPN_100ml_1': i[8],
+        'LosR': i[9],
+        'Silica': i[10],
+        'Ca': i[11],
+        'Mg': i[12],
+        'Ca_Mg': i[13],
+        'NO3': i[14],
+        'NO3_USGS': i[15],
+        'Cl2': i[16],
+        'Na': i[17],
+        'SO4': i[18],
+        'K': i[19],
+        'pH': i[20],
+        'Alk': i[21],
+        'Hard': i[22],
+        'Nhard': i[23],
+        'C_USGSTemp': i[24],
+        'F_USGS': i[25],
+        'Temp': i[26],
+        'MD_Precip_inch_mon': i[27],
+        'MD_Temp_F': i[28]
+    })
+
+#print(record)

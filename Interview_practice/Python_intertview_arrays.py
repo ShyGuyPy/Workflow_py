2efd9645387a18b493f4063fbaddfbe5bd62006d
ShyGuyPy <luke.vawter1@gmail.com>
2021-05-04 13:54:55 -0400

    test run of 2to3, for converting LFFS Python custom scripts from Python 2 to Python 3

---
+++ Interview_practice/Python_intertview_arrays.py
@@ -0,0 +1,4 @@
+#question from 'Elements of Programming Interview in Python'
+#Chapter 5 pg. 41
+#Arrays
+
---
+++ LFFS_Python_2to3/instructions.py
@@ -0,0 +1,7 @@
+###
+#pip install
+
+#move to directory C:\Python27\ArcGIS10.5\Tools\Scripts
+
+#example: 2to3.py --output-dir=C:\Users\lvawter\Documents\Python_scripts\Workflow_py\LFFS_Python_2to3\P3_output -W -n C:\Users\lvawter\Documents\Python_scripts\Workflow_py\LFFS_Python_2to3\P2_input\RetrieveHEFS.py
+#2to3 --output-dir=LFFS_Python_2to3\P2_output\RetrieveHEFS.py -W -n LFFS_Python_2to3\P2_input\RetrieveHEFS.py

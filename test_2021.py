8de3b8108a5e29252e2a4d5e288e195979f9f26b
ShyGuyPy <luke.vawter1@gmail.com>
2021-06-02 13:36:28 -0400

    removed some scripts

---
+++ test_2021.py
@@ -0,0 +1,83 @@
+# print("it's been a while...")
+#
+# test = (1,2,3)
+#
+# #print(test)
+#
+# def test_function(array):
+#
+#   print(array)
+#
+# test_function(test)
+#
+# class test_class:
+#   def __init__(self, test_value):
+#     self.value = test_value
+#
+# test_instantiation = test_class(42)
+#
+# print(test_instantiation.value)
+
+# def test_func(n):
+#   product=1
+#   my_sum = 0
+#   difference = 0
+#   n_str = str(n)
+#   for i in n_str:
+#     product = int(i) * product
+#     # print(product)
+#     my_sum = int(i) + my_sum
+#     # print(sum)
+#   difference = product - my_sum
+#   # print(difference)
+#   return difference
+
+  #   print(int(i) * i[+1])
+  #print((n*n) - (n+n))
+#
+# test_val = test_func(123456)
+# print(test_val)
+
+
+
+# def binaryPatternMatching(pattern, s):
+#   vowels = ('a','e','i','o','u','y')
+#   match_count = 0
+#
+#
+#   if len(pattern) == len(s):
+#     same_length = True
+#     for i in str(pattern):
+#       #if (len(i) == len(str(s[pattern.index(i)]))):
+#
+#
+#       if (int(i) == 0) & (s[pattern.index(i)] in vowels):
+#         vowel_match = True
+#         #print('vowel match')
+#       elif (int(i) == 1) & (s[pattern.index(i)] not in vowels):
+#         cons_match = True
+#         #print('con match')
+#         match_count += 1
+#
+#       else:
+#         match = match
+#         #print('no match')
+#       #print(match_count)
+#     return(match_count)
+#
+#
+#
+# result = binaryPatternMatching('1011','team')
+#
+# print(result)
+
+mat = [0,1,0,0],
+[1,1,1,0],
+[0,1,1,1],
+[0,0,1,0]
+
+
+
+
+
+
